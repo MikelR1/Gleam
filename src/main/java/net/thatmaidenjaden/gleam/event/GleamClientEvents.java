8e8e18cfcf4c621e600cf7125a11156d360b3f4f
MikelR1 <michaeldrnacho@gmail.com>
2026-09-09 05:59:10 -0700

    Initial fabric port

--- src/main/java/net/thatmaidenjaden/gleam/event/GleamClientEvents.java
+++ src/main/java/net/thatmaidenjaden/gleam/event/GleamClientEvents.java
@@ -1,17 +1,11 @@
 package net.thatmaidenjaden.gleam.event;
 
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.client.event.RegisterShadersEvent;
-import net.thatmaidenjaden.gleam.Gleam;
+import net.fabricmc.fabric.api.client.rendering.v1.CoreShaderRegistrationCallback;
 import net.thatmaidenjaden.gleam.client.lighting.GleamLightEngine;
 
-@EventBusSubscriber(value = Dist.CLIENT, modid = Gleam.MOD_ID)
 public class GleamClientEvents {
 
-    @SubscribeEvent
-    public static void onShaderRegistration(RegisterShadersEvent event) {
-        GleamLightEngine.getInstance().rebindBlocks();
+    public static void onShaderRegistration() {
+        CoreShaderRegistrationCallback.EVENT.register(context -> GleamLightEngine.getInstance().rebindBlocks());
     }
 }

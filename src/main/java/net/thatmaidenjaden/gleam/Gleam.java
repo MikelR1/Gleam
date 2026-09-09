21718f51b4eae91965e821b2b0e03fb8be31fd2f
MikelR1 <michaeldrnacho@gmail.com>
2026-09-09 05:59:10 -0700

    Initial fabric port

--- src/main/java/net/thatmaidenjaden/gleam/Gleam.java
+++ src/main/java/net/thatmaidenjaden/gleam/Gleam.java
@@ -1,18 +1,15 @@
 package net.thatmaidenjaden.gleam;
 
+import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.ResourceLocation;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mod(Gleam.MOD_ID)
-public final class Gleam {
+public final class Gleam implements ModInitializer {
     public static final String MOD_ID = "gleam";
     public static final Logger LOGGER = LoggerFactory.getLogger("Gleam");
 
-    public Gleam(IEventBus modEventBus, ModContainer modContainer) {
+    public void onInitialize() {
 
     }
 
--- src/main/java/net/thatmaidenjaden/gleam/GleamClient.java
+++ src/main/java/net/thatmaidenjaden/gleam/GleamClient.java
@@ -1,17 +1,16 @@
 package net.thatmaidenjaden.gleam;
 
-import foundry.veil.platform.VeilEventPlatform;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.loading.FMLLoader;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.thatmaidenjaden.gleam.client.patcher.GleamVeilPreProcessor;
+import net.thatmaidenjaden.gleam.event.GleamClientEvents;
+import net.thatmaidenjaden.gleam.event.GleamEvents;
 
-@Mod(value = Gleam.MOD_ID, dist = Dist.CLIENT)
-public final class GleamClient {
+public final class GleamClient implements ClientModInitializer {
 
-    public GleamClient(IEventBus modEventBus, ModContainer modContainer) {
-        if (FMLLoader.getLoadingModList().getModFileById("veil") != null) GleamVeilPreProcessor.initializePatch();
+    public void onInitializeClient() {
+        if (FabricLoader.getInstance().getModContainer("veil").isPresent()) GleamVeilPreProcessor.initializePatch();
+        GleamEvents.registerReloadListener();
+        GleamClientEvents.onShaderRegistration();
     }
 }

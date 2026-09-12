d5bb0ef619bdf0af0a22ef39253ad369f7e78dec
MikelR1 <michaeldrnacho@gmail.com>
2026-09-12 14:19:52 -0700

    Remove logging from testing configs

--- src/main/java/net/thatmaidenjaden/gleam/Gleam.java
+++ src/main/java/net/thatmaidenjaden/gleam/Gleam.java
@@ -3,7 +3,6 @@ package net.thatmaidenjaden.gleam;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.ResourceLocation;
 import net.thatmaidenjaden.gleam.config.GleamConfigImpl;
-import net.thatmaidenjaden.gleam.config.GleamConfigs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +12,6 @@ public final class Gleam implements ModInitializer {
 
     public void onInitialize() {
         GleamConfigImpl.init();
-        LOGGER.info(String.valueOf(GleamConfigs.ENABLE_COLORED_LIGHTS.get()));
     }
 
     public static ResourceLocation gleamPath(String path) {
